1eb5c9ec35c8ff99c1d8d52a3c5e67bda2ac6b01
Nguyen Quang Huy <qhuy20042005@gmail.com>
2026-03-11 23:51:46 +0700

    remove wrong BE and FE folders

--- BE/KingStep/TestByte.java
+++
@@ -1,7 +0,0 @@
-public class TestByte {
-    public static void main(String[] args) {
-        byte b = -1;
-        System.out.println("Result without mask: " + String.format("%02x", b));
-        System.out.println("Result with mask: " + String.format("%02x", b & 0xff));
-    }
-}
--- BE/KingStep/src/main/java/com/example/backend/repository/VoucherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.Voucher;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VoucherRepository extends JpaRepository<Voucher, Integer> {
-}
